7310071e8bd1429af9593451765bc57a25b67045
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2025-03-22 03:40:35 +0100

    Fix alias and path resolution issues

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
